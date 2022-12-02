627a44c9bddf8885a3451c8be1800fa119338427
DanielHernandez37 <710113@student.dusd.net>
2022-12-02 11:44:50 -0800

    Made Progress in Ruby Adventure

--- RubysAdventureHernandez/Assets/Scripts/RubyController.cs
+++ RubysAdventureHernandez/Assets/Scripts/RubyController.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //create 2 variables to use the unity builtt in axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        //create my movement vector cariable
-        Vector2 position = transform.position;
-
-        //create the movement of my character
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //Set the new position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
